f65a6ffd4c5d8ffecd498a6da0ee8842b9d817c5
Xavier Mayeur <xavier@mayeur.be>
2023-08-17 15:24:58 +0200

    add home assistant topic

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import json
-
 import wifiCfg
 from m5stack import *
 from m5ui import *
 from uiflow import *
 from machine import WDT
 from secret import *
-
-lcd.setRotation(1)
 import ntptime
 from m5mqtt import M5mqtt
 import hat
 
+lcd.setRotation(1)
 setScreenColor(0x111111)
 
 hat_env2_0 = hat.get(hat.ENV2)
@@ -34,7 +32,7 @@ def fun_m5stack_setConfig_(topic_data):
 wdt = WDT(timeout=20000)
 setScreenColor(0x000000)
 ntp = ntptime.client(host='de.pool.ntp.org', timezone=2)
-lcd.print((ntp.formatDatetime('dd-mm-yy', 'hh:mm')), 1, 1, 0x33cc00)
+# lcd.print((ntp.formatDatetime('dd-mm-yy', 'hh:mm')), 1, 1, 0x33cc00)
 m5mqtt = M5mqtt('EnvHat', mqtt_host, mqtt_port, mqtt_user, mqtt_password, 300)
 m5mqtt.subscribe(str('m5stack/setConfig'), fun_m5stack_setConfig_)
 m5mqtt.start()
@@ -57,6 +55,9 @@ while True:
     m5mqtt.publish(str('m5stack/humidity'), str(hum), 0)
     m5mqtt.publish(str('m5stack/pressure'), str(press), 0)
     m5mqtt.publish(str('m5stack/config'), str(config), 0)
+    # Home Assistant integration
+    payload = {"temperature": str(("%.1f" % (temp))), "humidity": str(("%.1f" % (hum))), "pressure": str(("%.0f" % (press)))}
+    m5mqtt.publish(str("homeassistant/sensor/m5stick/state"), json.dumps(payload))
     lcd.print(((str(' Baro: ') + str(("%.0f" % (press))))), 1, 10, 0xffffff)
     lcd.print(((str(' Temp: ') + str(("%.1f" % (temp))))), 1, 30, 0xffffff)
     lcd.print(((str(' Hum: ') + str(("%.1f" % (hum))))), 1, 50, 0xffffff)
